29f3975cc7bc274d588dada98444af6d838a3eb4
Fromant <framanchannel@gmail.com>
2025-02-01 16:59:31 +0300

    IL_utils.h print enhanced

--- src/util/IL_utils.h
+++ src/util/IL_utils.h
@@ -7,9 +7,28 @@
 
 namespace eraxc::IL {
 
+    std::string operand_to_string(const IL_operand &op) {
+        std::string result{};
+        if (!op.is_instant) result += '$';
+        result += std::to_string(op.id);
+        return result;
+    }
+
     void print_IL_nodes(const std::vector<IL_node> &nodes) {
         for (const auto &node: nodes) {
+            if (node.op == IL_node::ASSIGN) {
+                std::cout << node.assignee_type << ' ' << '$' << node.assignee << '='
+                          << operand_to_string(node.operand1) << std::endl;
+            } else if (node.op == IL_node::ADD) {
+                std::cout << node.assignee_type << ' ' << '$' << node.assignee << '='
+                          << operand_to_string(node.operand1) << '+'
+                          << operand_to_string(node.operand2) << std::endl;
+            } else if (node.op == IL_node::SUB) {
+                std::cout << node.assignee_type << ' ' << '$' << node.assignee << '='
+                          << operand_to_string(node.operand1) << '-'
+                          << operand_to_string(node.operand2) << std::endl;
 
+            }
         }
     }
 
@@ -20,9 +39,9 @@ namespace eraxc::IL {
             if (args_size > 0) {
                 for (int i = 0; i < args_size - 1; i++) {
                     const auto &decl = func.second.args[i];
-                    std::cout << decl.type << ' ' << decl.id << ", ";
+                    std::cout << decl.type << " $" << decl.id << ", ";
                 }
-                std::cout << func.second.args[args_size - 1].type << ' ' << func.second.args[args_size - 1].id
+                std::cout << func.second.args[args_size - 1].type << " $" << func.second.args[args_size - 1].id
                           << ") {\n";
             } else std::cout << ") {\n";
 
